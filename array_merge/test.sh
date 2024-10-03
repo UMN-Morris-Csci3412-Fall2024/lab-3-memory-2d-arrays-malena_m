db641292a78f453354275612f959f5fcd09b779a
mahon479 <mahon479@morris.umn.edu>
2024-10-03 11:05:42 -0500

    Updated test.sh to run the correct compiling code

--- array_merge/test.sh
+++ array_merge/test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Compile mergesort.c and test_mergesort.c
-g++ -Wall -g -o array_merge_test array_merge.c array_merge_test.cpp -lgtest -pthread -std=c++0x
+g++ -Wall -g -o array_merge_test ../mergesort/mergesort.c array_merge.c array_merge_test.cpp -lgtest -pthread -std=c++0x
 
 # Check if the compilation was successful
 if [ $? -eq 0 ]; then
     echo "Compilation successful."
     # Run the executable
-    ./mergesort_test
+    ./array_merge_test
 else
     echo "Compilation failed."
 fi
